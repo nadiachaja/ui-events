cddad80a9b0f3e534429a0efee9049ec13e9fa57
nadiachaja <nadiagaja13@gmail.com>
2024-12-09 12:32:26 +0100

    JS toegepast bij design, labels, code en user

--- assets/script/main.js
+++ assets/script/main.js
@@ -6,3 +6,53 @@ interaction.addEventListener('animationend', jumpHandler)
 function jumpHandler() {
   interaction.classList.toggle('jump')
 }
+
+
+
+
+let bibber = document.querySelector('a:nth-of-type(2)')
+
+bibber.addEventListener('click', bibberHandler)
+bibber.addEventListener('animationend', bibberHandler)
+
+function bibberHandler(){
+  bibber.classList.toggle('bibberen')
+}
+
+
+
+let rotate = document.querySelector('a:nth-of-type(9)')
+
+rotate.addEventListener('click', rotateHandler)
+
+function rotateHandler(){
+  rotate.classList.toggle('rotate')
+}
+
+
+
+let grote = document.querySelector('a:nth-of-type(20)')
+
+grote.addEventListener('click', widthHandler)
+
+function widthHandler(){
+grote.classList.toggle('width')
+}
+
+
+let move = document.querySelector('a:nth-of-type(17)')
+
+move.addEventListener('click', squshHandler)
+
+function squshHandler(){
+  move.classList.toggle('sqush')
+  
+}
+
+
+let shift = document.querySelector('a:nth-of-type(8)')
+
+ 
+
+
+
